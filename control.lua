977aca2ed4fa6b5c2e6cfaac9a65f5c6cc8dc70d
wadeack <wdckrpdg@gmail.com>
2025-12-25 13:20:56 +0100

    Modifier fixed, v.0.1.3

--- control.lua
+++ control.lua
@@ -1,8 +1,9 @@
 -- This file is part of belt-immunity-with-benefits mod
 -- Licensed under the MIT License. See LICENSE file for details.
 
+original_speed_modifier = 0
 speed_is_modified = false
-revert_modifier = 0
+--revert_modifier = 0
 modifier_to_avoid = 1
 
 
@@ -41,8 +42,6 @@ function find_entity_by_type(player, entity_type)
     return find_entities_filtered(player, entity_type, left_bound, right_bound)
 end
 
-
-
 function check_for_belt_immunity(player)
     for _, equipment in pairs(player.character.grid.equipment) do
         if equipment.name == 'belt-immunity-equipment' and equipment.energy > 0 then
@@ -58,8 +57,13 @@ script.on_event(defines.events.on_player_changed_position,
         local player = game.get_player(event.player_index)
         if player.controller_type == defines.controllers.character and player.character.grid then
             local has_immunity = check_for_belt_immunity(player)
+            if player.character_running_speed_modifier ~= nil then
+                if speed_is_modified == false then
+                    original_speed_modifier = player.character_running_speed_modifier
+                end
+            end
 --            player.print("Running speed: " .. tostring(player.character_running_speed))
---            player.print("Running speed modifier: " .. tostring(player.character_running_speed_modifier))
+--            player.print("Running speed modifier: " .. tostring(player.character_running_speed_modifier).."Original speed mod:"..tostring(original_speed_modifier))
             if has_immunity then
                 local belt = find_entity_by_type(player, "transport-belt")
                 if belt ~= nil then
@@ -68,22 +72,26 @@ script.on_event(defines.events.on_player_changed_position,
                         local belt_speed = belt.prototype.belt_speed
 --                        player.print("Belt speed: " .. tostring(belt_speed))
 
-                        local modifier = (player.character_running_speed + belt_speed)/player.character_running_speed
---                        player.print("Modifier value: " .. tostring(modifier) .. ", Modifier to avoid: " .. tostring(modifier_to_avoid))
+                        local base_speed_before_last_modifier = player.character_running_speed/(modifier_to_avoid + original_speed_modifier)
+
+--                        player.print("Speed check: " .. tostring(base_speed_before_last_modifier))
+                        local modifier = (base_speed_before_last_modifier + belt_speed)/base_speed_before_last_modifier
+--                        player.print("Modifier value: " .. tostring(modifier) .. ", Modifier to avoid: " .. tostring(modifier_to_avoid) .. ", Modifier to be applied: " .. tostring(modifier - 1 + original_speed_modifier))
                         is_different_modifier = (modifier ~= modifier_to_avoid)
-                        if speed_is_modified == false or is_different_modifier then
+                        if is_different_modifier then
 --                            player.print("Speed will be modified.")
-                            modifier_to_avoid = (player.character_running_speed*modifier + belt_speed)/(player.character_running_speed*modifier)
-                            -- assuming there is no speed modifier for character
-                            player.character_running_speed_modifier = modifier - 1
-                            revert_modifier = 0
+
+                            modifier_to_avoid = modifier
+                            player.character_running_speed_modifier = modifier - 1 + original_speed_modifier
                             speed_is_modified = true
                         end
                     end
                 else
                     if speed_is_modified == true then
-                        player.character_running_speed_modifier = player.character_running_speed_modifier * revert_modifier
+                        player.character_running_speed_modifier = original_speed_modifier
+--                        player.character_running_speed_modifier = player.character_running_speed_modifier * revert_modifier
                         speed_is_modified = false
+                        modifier_to_avoid = 1
 --                        player.print("Speed has been reset")
                     end
                 end
